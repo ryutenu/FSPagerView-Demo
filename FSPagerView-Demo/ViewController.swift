22b19c5f1da246b5e31ba5f48c4ee5eeb7a3de5f
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-04 21:02:27 +0900

    [Mod] Add func setupPagerView()

--- FSPagerView-Demo/ViewController.swift
+++ FSPagerView-Demo/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import FSPagerView
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pagerView: FSPagerView!
+    
+    private let pageCount = 6
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupPagerView()
     }
-
-
+    
+    private func setupPagerView() {
+        pagerView.dataSource = self
+        pagerView.delegate = self
+        pagerView.register(FSPagerViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        pagerView.isInfinite = true
+        pagerView.itemSize = CGSize(width: 200, height: 50)
+        pagerView.interitemSpacing = 20
+        pagerView.transformer = FSPagerViewTransformer(type: .overlap)
+    }
 }
 
+extension ViewController: FSPagerViewDataSource {
+    func numberOfItems(in pagerView: FSPagerView) -> Int {
+        pageCount
+    }
+    
+    func pagerView(_ pagerView: FSPagerView, cellForItemAt index: Int) -> FSPagerViewCell {
+        let cell = pagerView.dequeueReusableCell(withReuseIdentifier: "cell", at: index)
+        cell.textLabel?.text = "Page" + String(index)
+        return cell
+    }
+}
+
+extension ViewController: FSPagerViewDelegate {
+    
+}
