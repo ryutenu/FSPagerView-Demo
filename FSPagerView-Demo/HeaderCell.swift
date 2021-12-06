24a93592eecb79b917498a50ac558ddf9d41e7bb
Alan Liu <ryutenu.tokyo@gmail.com>
2021-12-06 13:48:26 +0900

    [Mod] Cell design

--- FSPagerView-Demo/HeaderCell.swift
+++ FSPagerView-Demo/HeaderCell.swift
@@ -13,18 +13,17 @@ class HeaderCell: FSPagerViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     
     private let titleList = ["千早ぶる", "神代もきかず", "竜田川", "からくれなゐに", "水くくるとは"]
-    private let colorList = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue]
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func configureCell(index: Int) {
-        guard titleList.indices.contains(index), colorList.indices.contains(index) else {
-            return
-        }
-        backgroundColor = colorList[index]
+        backgroundColor = .systemGray4
+        layer.borderColor = UIColor.systemBrown.cgColor
+        layer.borderWidth = 2
         
+        guard titleList.indices.contains(index) else { return }
         titleLabel.text = titleList[index]
     }
 }
